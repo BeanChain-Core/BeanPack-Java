4a6f5486b239078e61dbd8b8502674c90e646457
Sam Fawks <samfawk@limabean.xyz>
2025-07-02 21:57:13 -0400

    head version update

--- bean-core/src/main/java/com/beanpack/head.java
+++ bean-core/src/main/java/com/beanpack/head.java
@@ -2,6 +2,6 @@ package com.beanpack;
 
 //just for persistent variables in versioning 
 public class head {
-    public static String version = "BEANPACK-JAVA v0.0.1";
+    public static String version = "BEANPACK-JAVA v0.1.0";
     
 }
